7cb049aa81f38959b3b7841a97057ef5ba28e6b0
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-16 13:14:26 +0530

    code formatting and package lock file

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,14 @@ import {
   List,
   ListIcon,
   ListItem,
-} from '@chakra-ui/core'
+} from "@chakra-ui/core";
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Hero } from "../components/Hero";
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { CTA } from "../components/CTA";
+import { Footer } from "../components/Footer";
 
 const Index = () => (
   <Container>
@@ -50,6 +50,6 @@ const Index = () => (
     </Footer>
     <CTA />
   </Container>
-)
+);
 
-export default Index
+export default Index;
