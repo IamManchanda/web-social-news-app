454d90eb0bc7b9955795f8afc7bb2bf521e3207f
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-27 20:54:55 +0530

    Fix vote cache bug - headers cookie

--- src/utils/with-apollo.ts
+++ src/utils/with-apollo.ts
@@ -1,31 +1,39 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { withApollo as createWithApollo } from "next-apollo";
+import { createWithApollo } from "./create-with-apollo";
 import { PaginatedPostsResponse } from "../generated/graphql";
+import { NextPageContext } from "next";
 
 const { NEXT_PUBLIC_API_BACKEND_URL } = process.env;
-const client = new ApolloClient({
-  uri: NEXT_PUBLIC_API_BACKEND_URL as string,
-  credentials: "include",
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          posts: {
-            keyArgs: [],
-            merge(
-              existing: PaginatedPostsResponse | undefined,
-              incoming: PaginatedPostsResponse,
-            ): PaginatedPostsResponse {
-              return {
-                ...incoming,
-                posts: [...(existing?.posts || []), ...incoming.posts],
-              };
+const createClient = (ctx: NextPageContext) =>
+  new ApolloClient({
+    uri: NEXT_PUBLIC_API_BACKEND_URL as string,
+    credentials: "include",
+    headers: {
+      cookie:
+        (typeof window === "undefined"
+          ? ctx?.req?.headers.cookie
+          : undefined) || "",
+    },
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            posts: {
+              keyArgs: [],
+              merge(
+                existing: PaginatedPostsResponse | undefined,
+                incoming: PaginatedPostsResponse,
+              ): PaginatedPostsResponse {
+                return {
+                  ...incoming,
+                  posts: [...(existing?.posts || []), ...incoming.posts],
+                };
+              },
             },
           },
         },
       },
-    },
-  }),
-});
+    }),
+  });
 
-export const withApollo = createWithApollo(client);
+export const withApollo = createWithApollo(createClient);
